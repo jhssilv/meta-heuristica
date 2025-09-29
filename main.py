e7d0b9f49e5ecd488d63eeefa4b779657fc5a9e7
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-09-29 16:58:36 -0300

    added shake (swap)  and first improvement functions

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-import argparse
 
 from instance import Instance
 from input_parse import parse_args
 from solver import Solver
-
+import time
 
 
 if __name__ == "__main__":
     args = parse_args()
+    
+    max_iter = args.stopping_value
+    
     instance = Instance(args.input_file)
     solver = Solver(instance)
     
-    #print(solver.current_solution)
-    print(solver.is_solution_viable(solver.current_solution))
-    print(solver.evaluate_solution(solver.current_solution))
+    initial_value = solver.evaluate_solution(solver.current_solution)
+    print(f"Initial solution value: {initial_value}")
+
+    start_time = time.time()
+    for i in range(max_iter):
+        solver.first_improvement()
+        
+    final_value = solver.evaluate_solution(solver.current_solution)
+    print(f"Final solution value: {final_value}")
+    print(f"Done {i+1} iterations in {time.time() - start_time:.2f} seconds.")
     
--- solver.py
+++ solver.py
@@ -2,19 +2,49 @@ from instance import Instance
 from utils import distance
 from copy import deepcopy
 from collections import defaultdict
-
+from random import randint
 
 class Solver:
     def __init__(self, instance: Instance):
         self.instance = instance
         self.current_solution = self.generate_initial_solution()
+        self.current_solution_value = self.evaluate_solution(self.current_solution)
 
     ###########################
     #   Local Search Methods  #
     ###########################
+    
+    def swap(self, i:int, j:int):
+        self.current_solution[i], self.current_solution[j] = self.current_solution[j], self.current_solution[i]
+    
+    def shake(self):
+        swap_is_valid = False
+        while not swap_is_valid:
+            first_idx = randint(0, len(self.current_solution) - 1)
+            second_idx = randint(0, len(self.current_solution) - 1)
 
+            sol_first = self.current_solution[first_idx]
+            sol_second = self.current_solution[second_idx]
+
+            for i in range(first_idx, second_idx):
+                if sol_first in self.instance.get_prerequisites_for(self.current_solution[i]) or \
+                    self.current_solution[i] in self.instance.get_prerequisites_for(sol_second):
+                        break
+                else:
+                    swap_is_valid = True
+                    break
+                    
+        return (first_idx, second_idx)
+                        
     def first_improvement(self):
-        pass
+        
+        new_value = self.current_solution_value
+        
+        while(new_value >= self.current_solution_value):    
+            delta = self.shake()
+            new_value = self.evaluate_solution_delta(self.current_solution, delta)
+
+        self.swap(delta[0], delta[1])
 
     def best_improvement(self):
         pass
@@ -22,13 +52,14 @@ class Solver:
     ################################
     #   First Solution Generation  #
     ################################
-    
+            
     # Topological sort approach with Kahn's algorithm [https://en.wikipedia.org/wiki/Topological_sorting]
     def generate_initial_solution(self) -> list[int]:    
         prerequisites = deepcopy(self.instance.prerequisites)
         temples = self.instance.get_temples()
 
         # build reverse graph: t -> [who depends on t]
+        # this can be moved to instance if needed elsewhere
         dependents = defaultdict(list)
         for t, prereqs in prerequisites.items():
             for p in prereqs:
@@ -69,11 +100,66 @@ class Solver:
     def evaluate_solution(self, solution: list[int]) -> int:
         return sum(
             distance(
-                self.instance.get_temple(solution[i]), 
-                self.instance.get_temple(solution[i+1])
+                self.instance.get_temple(solution[i]),
+                self.instance.get_temple(solution[i + 1])
             )
             for i in range(len(solution) - 1)
             )
+
+    def evaluate_solution_delta(self, solution: list[int], delta: tuple[int, int]) -> int:
+        sol_value = self.current_solution_value
+        
+        # First we remove the distances from the edges that will be changed        
+        
+        if delta[0] < len(solution) - 1:
+            sol_value -= distance(
+                self.instance.get_temple(solution[delta[0]]),
+                self.instance.get_temple(solution[delta[0] + 1])
+            )
+        
+        if delta[1] < len(solution) - 1:    
+            sol_value -= distance(
+                self.instance.get_temple(solution[delta[1]]),
+                self.instance.get_temple(solution[delta[1] + 1])
+            )
+        
+        if delta[0] > 0:
+            sol_value -= distance(
+                self.instance.get_temple(solution[delta[0]]),
+                self.instance.get_temple(solution[delta[0] - 1])
+            )
+
+        if delta[1] > 0:
+            sol_value -= distance(
+                self.instance.get_temple(solution[delta[1]]),
+                self.instance.get_temple(solution[delta[1] - 1])
+            )    
+        
+    # Then we add the distances from the new edges created by the swap
+        if delta[0] < len(solution) - 1:    
+            sol_value += distance(
+                self.instance.get_temple(solution[delta[1]]),
+                self.instance.get_temple(solution[delta[0] + 1])
+            )
+        if delta[1] < len(solution) - 1:    
+            sol_value += distance(
+                self.instance.get_temple(solution[delta[0]]),
+                self.instance.get_temple(solution[delta[1] + 1])
+            )
+
+        if delta[0] > 0:
+            sol_value += distance(
+                self.instance.get_temple(solution[delta[1]]),
+                self.instance.get_temple(solution[delta[0] - 1])
+            )
+            
+        if delta[1] > 0:
+            sol_value += distance(
+                self.instance.get_temple(solution[delta[0]]),
+                self.instance.get_temple(solution[delta[1] - 1])
+            )
+            
+        return sol_value
 
     ############################
     # Solution viability check #
@@ -95,15 +181,10 @@ class Solver:
         for temple in solution[1:]:
             for prereq in self.instance.get_prerequisites_for(temple):
                 if not visited[prereq]:
-                    print(f"Temple {temple} visited before its prerequisite {prereq}.")
                     return False
             visited[temple] = True
             
-        print('\n')
         if not all(visited.values()):
-            for temple, was_visited in visited.items():
-                if not was_visited:
-                    print(f"Temple {temple} was never visited.")
             return False            
         
         return True
